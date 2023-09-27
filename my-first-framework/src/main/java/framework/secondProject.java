8d6994d6a29f63a5607afe2ff1e86226404c5522
manasi110187 <manasi.tilwalli@gmail.com>
2023-09-27 13:57:49 -0500

    2nd project

---
+++ my-first-framework/src/main/java/framework/secondProject.java
@@ -0,0 +1,17 @@
+package framework;
+
+public class secondProject {
+	
+	public void sum(int i, int j)
+	{
+		System.out.println(i+j);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		secondProject sp= new secondProject();
+		sp.sum(8, 9);
+
+	}
+
+}
