4502f8ba91a994f90cc5909631405dfea40fff42
manasi110187 <manasi.tilwalli@gmail.com>
2023-09-27 23:08:25 -0500

    3 digit addition, code changes

--- my-first-framework/src/main/java/framework/secondProject.java
+++ my-first-framework/src/main/java/framework/secondProject.java
@@ -2,15 +2,15 @@ package framework;
 
 public class secondProject {
 	
-	public void sum(int i, int j)
+	public void sum(int i, int j, int k)
 	{
-		System.out.println(i+j);
+		System.out.println(i+j+k);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		secondProject sp= new secondProject();
-		sp.sum(8, 9);
+		sp.sum(8, 9, 9);
 
 	}
 
